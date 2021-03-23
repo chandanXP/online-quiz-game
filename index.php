7fe74d1d2f9972ed64e91ca62c54d3f13d82df36
chandan <chk33me@gmail.com>
2021-03-23 18:19:50 +0530

    adding css

--- index.php
+++ index.php
@@ -1,13 +1,87 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+     <link rel="stylesheet" href="./sass/main.css">
      <title>Online Quiz</title>
 </head>
+
 <body>
-     <h1>Hello World!</h1>
-     <?php include './Components/component.php'?>
+     <div class="container">
+          <div class="nav">
+               <h1>Online Quiz</h1>
+          </div>
+          <div class="row row-cols-2 body">
+               <!-- <?php include './Components/component.php' ?> -->
+
+               <div class="card mx-4 mb-4" style="max-width: 500px;">
+                    <div class="row g-0">
+                         <div class="col-md-4">
+                              <img src="https://i1.wp.com/www.elkovan.com.sg/wp-content/uploads/2019/02/Math-Square-Logo.png?ssl=1" alt="...">
+                         </div>
+                         <div class="col-md-8">
+                              <div class="card-body mx-4">
+                                   <h5 class="card-title">Maths Quiz</h5>
+                                   <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                   <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+               <div class="card  mx-4 mb-4" style="max-width: 500px;">
+                    <div class="row g-0">
+                         <div class="col-md-4 quizIcon">
+                              <img src="https://th.bing.com/th/id/R664a26675976fc8fe694ad9892806b82?rik=B%2fxgH63jsVWycg&riu=http%3a%2f%2fmandoraschihouse.co.uk%2fwp-content%2fuploads%2f2015%2f11%2fknowledge_icon.png&ehk=ipGT0D7ldofIg7%2bCr0BVMXdPXYAYXYXKi237tTCSTLQ%3d&risl=&pid=ImgRaw" alt="...">
+                         </div>
+                         <div class="col-md-8">
+                              <div class="card-body mx-4">
+                                   <h5 class="card-title">General Knowledge Quiz</h5>
+                                   <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                   <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+               <div class="card  mx-4 mb-4" style="max-width: 500px;">
+                    <div class="row g-0">
+                         <div class="col-md-4">
+                              <img src="..." alt="...">
+                         </div>
+                         <div class="col-md-8">
+                              <div class="card-body mx-4">
+                                   <h5 class="card-title">Cricket Quiz</h5>
+                                   <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                   <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+               <div class="card mx-4 mb-4" style="max-width: 500px;">
+                    <div class="row g-0">
+                         <div class="col-md-4">
+                              <img src="..." alt="...">
+                         </div>
+                         <div class="col-md-8">
+                              <div class="card-body mx-4">
+                                   <h5 class="card-title">Animals Quiz</h5>
+                                   <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                   <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+
+          </div>
+          <div class="footer">
+               <h4>-FOOTER-</h4>
+          </div>
+     </div>
 </body>
+
 </html>
